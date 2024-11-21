efb91beb4013395ca7f4724a0edfdd4642726837
dangph12 <dangphhe181217@fpt.edu.vn>
2024-11-21 10:28:22 +0700

    feat: Test api working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import axios from 'axios';
+import {useEffect} from 'react';
 function App(){
+    useEffect(() => {
+        axios.get('http://localhost:9999/recipes')
+        .then(response => {
+            console.log(response.data);
+        })
+    }, [])
     return (
         <div>Hello World</div>
     )
