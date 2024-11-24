cb76c11a452c5f5cded2c0bb4cb85ccef8045d14
dangph12 <dangphhe181217@fpt.edu.vn>
2024-11-24 09:10:42 +0700

    feat: Change to products

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect} from 'react';
 function App(){
     const url = process.env.REACT_APP_API_PATH;
     useEffect(() => {
-        axios.get(`${url}/recipes`)
+        axios.get(`${url}/products`)
         .then(response => {
             console.log(response.data);
         })
