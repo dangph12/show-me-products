045b9fb73ac6ddac35a26b9b20547b5e93733f9e
Trang Vu <trangvlhe181187@fpt.edu.vn>
2024-11-26 13:42:02 +0700

    fix: delete comment

--- src/pages/ProductDisplay.jsx
+++ src/pages/ProductDisplay.jsx
@@ -1,54 +1,3 @@
-// import React, { lazy, Suspense, useContext } from "react";
-// import SearchBar from "../components/SearchBar";
-// import Loading from "../components/Loading";
-// import ListGroup from "react-bootstrap/ListGroup";
-// import { Card, Col, Row } from "react-bootstrap";
-// import { ProductsContext } from "../contexts/ProductsContext";
-
-
-
-// const ProductList = lazy(() => import("../components/ProductList"));
-
-// function ProductDisplay() {
-//   const { products } = useContext(ProductsContext);
-
-//   return (
-//     <div className="container">
-//       <Suspense fallback={<Loading />}>
-//         <SearchBar />
-//         <Row className="mt-4">
-//           {products.map((pro, index) => (
-//             <Col>
-//               <Card style={{ width: "18rem", marginBottom: "20px" }}>
-//                 <Card.Img variant="top" src={pro.images} />
-//                 <Card.Body>
-//                   <Card.Title>Card Title</Card.Title>
-//                   <Card.Text>
-//                     Some quick example text to build on the card title and make
-//                     up the bulk of the card's content.
-//                   </Card.Text>
-//                 </Card.Body>
-//                 <ListGroup className="list-group-flush">
-//                   <ListGroup.Item>Cras justo odio</ListGroup.Item>
-//                   <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-//                   <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-//                 </ListGroup>
-//                 <Card.Body>
-//                   <Card.Link href="#">Card Link</Card.Link>
-//                   <Card.Link href="#">Another Link</Card.Link>
-//                 </Card.Body>
-//               </Card>
-//             </Col>
-//           ))}
-//         </Row>
-//       </Suspense>
-//     </div>
-//   );
-// }
-
-// export default ProductDisplay;
-
-
 import React, { lazy, Suspense, useContext, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import Loading from "../components/Loading";
@@ -94,7 +43,8 @@ function ProductDisplay() {
                 <ListGroup className="list-group-flush">
                   <ListGroup.Item>Price: ${pro.price}</ListGroup.Item>
                   <ListGroup.Item>Category: {pro.category}</ListGroup.Item>
-                  <ListGroup.Item>Category: {pro.tags.join(",")}</ListGroup.Item>
+                  <ListGroup.Item>Tag: {pro.tags.join(",")}</ListGroup.Item>
+                  <ListGroup.Item>Brand: {pro.brand}</ListGroup.Item>
                 </ListGroup>
                 <Card.Body>
                   <Button href="#">Details</Button>
