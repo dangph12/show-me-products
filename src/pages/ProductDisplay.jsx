9b58777afde56f6baa2621ba6323c011f847fc20
dangph12 <dangphhe181217@fpt.edu.vn>
2024-11-26 11:26:52 +0700

    feat: Add filter

--- src/pages/ProductDisplay.jsx
+++ src/pages/ProductDisplay.jsx
@@ -1,15 +1,23 @@
 import React, { lazy, Suspense } from "react";
 import SearchBar from "../components/SearchBar";
 import Loading from "../components/Loading";
+import SingleFilter from "../components/SingleFilter";
+import MultiFilters from "../components/MultiFilters";
 
-const ProductList = lazy(() => import("../components/ProductList"))
+const ProductList = lazy(() => import("../components/ProductList"));
+const ProductGrid = lazy(() => import("../components/ProductGrid"));
+const ProductTable = lazy(() => import("../components/ProductTable"));
+
 
 function ProductDisplay() {
   return (
-    <Suspense fallback={<Loading />}>
+    <>
       <SearchBar />
-      <ProductList />
-    </Suspense>
+      <SingleFilter />
+      <Suspense fallback={<Loading />}>
+        <ProductTable />
+      </Suspense>
+    </>
   );
 }
 
