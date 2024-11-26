bad0fecd495debca4370b5d42ef41e16a57d585a
lnvh <lenguyenviethung1509@gmail.com>
2024-11-26 14:43:15 +0700

    feat: cart context

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { CartContext } from '../contexts/CartContext'
+import { useContext } from 'react'
 function Cart() {
+  const { cart } = useContext(CartContext);
   return (
-    <div>Cart</div>
+    <>
+    {cart.map((product) => (
+      <div key={product.id}>
+        <img src={product.images[0]} alt={product.title} />
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+      </div>
+      
+    ))}
+    </>
   )
 }
 
