27c444ce6ca278df6a6658868292226c991ddc17
dangph12 <dangphhe181217@fpt.edu.vn>
2024-11-26 11:26:25 +0700

    feat: Add context

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import SearchProvider from "../contexts/SearchContext";
 import ProductsProvider from "../contexts/ProductsContext";
+import CategoriesProvider from "../contexts/CategoriesContext";
+import SingleFilterProvider from "../contexts/SingleFilterContext";
+import MultiFiltersProvider from "../contexts/MultiFiltersContext";
 
 function AppWrapper({ children }) {
   return (
     <ProductsProvider>
-      <SearchProvider>{children}</SearchProvider>
+      <CategoriesProvider>
+        <SearchProvider>
+          <MultiFiltersProvider>
+            <SingleFilterProvider>{children}</SingleFilterProvider>
+          </MultiFiltersProvider>
+        </SearchProvider>
+      </CategoriesProvider>
     </ProductsProvider>
   );
 }
