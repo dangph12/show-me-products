63f830c127228c901e7f14a9eea7fead29b138b5
lnvh <lenguyenviethung1509@gmail.com>
2024-11-26 14:42:43 +0700

    Feat: product display

--- src/pages/ProductDisplay.jsx
+++ src/pages/ProductDisplay.jsx
@@ -3,6 +3,7 @@ import SearchBar from "../components/SearchBar";
 import Loading from "../components/Loading";
 import SingleFilter from "../components/SingleFilter";
 import MultiFilters from "../components/MultiFilters";
+import { Container } from "react-bootstrap";
 
 const ProductList = lazy(() => import("../components/ProductList"));
 const ProductGrid = lazy(() => import("../components/ProductGrid"));
@@ -12,10 +13,12 @@ const ProductTable = lazy(() => import("../components/ProductTable"));
 function ProductDisplay() {
   return (
     <>
+
       <SearchBar />
+
       <SingleFilter />
       <Suspense fallback={<Loading />}>
-        <ProductTable />
+        <ProductGrid />
       </Suspense>
     </>
   );
