98ef37f85c39708eb5f4fe1f864cfbbd58308ddc
dangph12 <dangphhe181217@fpt.edu.vn>
2024-11-29 10:48:15 +0700

    feat: Create layout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,45 +1,121 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { OrderContext } from "../contexts/OrderContext";
 import { OrderHistoryContext } from "../contexts/OrderHistoryContext";
 import { CartContext } from "../contexts/CartContext";
+import { Container, Col, Row, Button } from "react-bootstrap";
 
 function Checkout() {
+  const [isFulfilled, setIsFulfilled] = useState(false);
   const { order, setOrder } = useContext(OrderContext);
   const { orderHistory, setOrderHistory } = useContext(OrderHistoryContext);
   const { setCart } = useContext(CartContext);
 
+  useEffect(() => {
+    if (
+      order.customer.name !== "" &&
+      order.customer.address !== "" &&
+      order.customer.phone !== "" &&
+      order.products.length > 0
+    ) {
+      setIsFulfilled(true);
+    } else {
+      setIsFulfilled(false);
+    }
+  }, [order, setOrder]);
+
   return (
-    <>
-      <h1>Checkout</h1>
-      <p>Name: {order.customer.name}</p>
-      <p>Address: {order.customer.address}</p>
-      <p>Phone: {order.customer.phone}</p>
-      <h2>Products</h2>
-      <ul>
-        {order.products.map((product) => (
-          <li key={product.id}>
-            {product.title} - {product.price}
-          </li>
-        ))}
-      </ul>
-      <button
-        onClick={() => {
-          setOrderHistory([...orderHistory, order]);
-          setOrder({
-            customer: {
-              name: "",
-              address: "",
-              phone: "",
-            },
-            products: [],
-          });
-          setCart((prevCart) => prevCart.filter((cartProduct) => !order.products.includes(cartProduct)));
-        }}
-      >
-        Place Order
-      </button>
-    </>
-  )
+    <Container fluid className="px-5 mt-4">
+      <Row>
+        <Col>
+          <h2>Customer Information</h2>
+          <form>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={order.customer.name}
+                onChange={(e) =>
+                  setOrder({
+                    ...order,
+                    customer: { ...order.customer, name: e.target.value },
+                  })
+                }
+              />
+            </label>
+            <label>
+              Address:
+              <input
+                type="text"
+                value={order.customer.address}
+                onChange={(e) =>
+                  setOrder({
+                    ...order,
+                    customer: { ...order.customer, address: e.target.value },
+                  })
+                }
+              />
+            </label>
+            <label>
+              Phone:
+              <input
+                type="text"
+                value={order.customer.phone}
+                onChange={(e) =>
+                  setOrder({
+                    ...order,
+                    customer: { ...order.customer, phone: e.target.value },
+                  })
+                }
+              />
+            </label>
+          </form>
+          <Button variant="warning" as={Link} to="/cart">
+            Back to Cart
+          </Button>
+          {isFulfilled ? (
+            <Button
+              variant="success"
+              onClick={() => {
+                setOrderHistory([...orderHistory, order]);
+                setOrder({
+                  customer: {
+                    name: "",
+                    address: "",
+                    phone: "",
+                  },
+                  products: [],
+                  total: 0,
+                });
+                setCart((prevCart) =>
+                  prevCart.filter(
+                    (cartProduct) => !order.products.includes(cartProduct)
+                  )
+                );
+              }}
+            >
+              Place Order
+            </Button>
+          ) : (
+            <Button variant="secondary" disabled>
+              Place Order
+            </Button>
+          )}
+        </Col>
+        <Col>
+          <h2>Products</h2>
+          <ul>
+            {order.products.map((product) => (
+              <li key={product.id}>
+                {product.title} - {product.price}
+              </li>
+            ))}
+          </ul>
+          <div>{order.total}</div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Checkout
+export default Checkout;
