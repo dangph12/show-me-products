26f67da3e2b563f51cf39cb53478ee6061b26ba1
lnvh <lenguyenviethung1509@gmail.com>
2024-12-10 15:12:50 +0700

    feat: Fix navbar

--- src/layout/includes/Header.jsx
+++ src/layout/includes/Header.jsx
@@ -1,15 +1,5 @@
 import React from "react";
-import { Container, Nav } from "react-bootstrap";
-import routes from "../../routes/index";
-import {
-  Navbar,
-  Form,
-  FormControl,
-  Button,
-  InputGroup,
-  Dropdown,
-  DropdownButton,
-} from "react-bootstrap";
+import { Container, Navbar, Nav, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 // import { FaBell, FaShoppingCart } from "react-icons/fa";
 
@@ -18,16 +8,29 @@ function Header() {
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
+          <Col>
+          <Link to="/">
           <img
             style={{ width: "100px" }}
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/EBay_logo.svg/800px-EBay_logo.svg.png"
           ></img>
-
-          {routes.map((r, idx) => (
+          </Link>
+          </Col>
+          <Col>
             <Nav className="me-auto">
-              <Nav.Link  key={idx + r.name} as={Link} to={r.path}>{r.name}</Nav.Link>
+              <Nav.Link  key="product" as={Link} to="/products">Product</Nav.Link>
             </Nav>
-          ))}
+            </Col>
+            <Col>
+            <Nav className="me-auto">
+              <Nav.Link  key="cart" as={Link} to="/cart">Cart</Nav.Link>
+            </Nav>
+            </Col>
+            <Col>
+            <Nav className="me-auto">
+              <Nav.Link  key="history" as={Link} to="/order-history">History</Nav.Link>
+            </Nav>
+            </Col>
         </Container>
       </Navbar>
     </div>
