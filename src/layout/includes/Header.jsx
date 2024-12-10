8f5f867373dbd95304da9dc8d6d9b757c3005bbb
Trang Vu <trangvlhe181187@fpt.edu.vn>
2024-12-10 14:02:48 +0700

    fix header

--- src/layout/includes/Header.jsx
+++ src/layout/includes/Header.jsx
@@ -15,24 +15,6 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    // <Nav className="container">
-    //   <Link to="/">
-    //     <img
-    //       src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/EBay_logo.svg/800px-EBay_logo.svg.png"
-    //       width="80"
-    //       alt="eBay Logo"
-    //     />
-    //   </Link>
-
-    //   {routes.map((route, idx) => (
-    //     <Nav.Item key={idx + route.name}>
-    //       <Nav.Link as={Link} to={route.path}>
-    //         {route.name}
-    //       </Nav.Link>
-    //     </Nav.Item>
-    //   ))}
-    // </Nav>
-
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
